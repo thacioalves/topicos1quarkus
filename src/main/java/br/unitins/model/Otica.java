0a22e8883057b288c35bf086d596115a1d637a78
Thacio Alves <118781681+thacioalves@users.noreply.github.com>
2023-02-17 20:07:06 -0300

    Create Repository

---
+++ src/main/java/br/unitins/model/Otica.java
@@ -0,0 +1,45 @@
+package br.unitins.model;
+
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+@Entity
+public class Otica extends PanacheEntity {
+    private String modelo;
+    private String marca;
+    private String lente;
+    private String fornecedor;
+    private String tamanho;
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getLente() {
+        return lente;
+    }
+    public void setLente(String lente) {
+        this.lente = lente;
+    }
+    public String getFornecedor() {
+        return fornecedor;
+    }
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+    public String getTamanho() {
+        return tamanho;
+    }
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+}
---
+++ src/main/java/br/unitins/resource/OticaResource.java
@@ -0,0 +1,32 @@
+package br.unitins.resource;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.unitins.model.Otica;
+
+@Path("/Oculos")
+public class OticaResource {
+    @GET
+    @Path("/todos")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Otica> getall(){
+        return Otica.findAll().list();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Otica inserir(Otica otica){
+        otica.persist();
+        return otica;
+    }
+}
