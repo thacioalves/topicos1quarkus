9deae5dbfb71381eef149d15bc62422224dacbe3
Thacio Alves <118781681+thacioalves@users.noreply.github.com>
2023-03-03 18:21:34 -0300

    update repository
    Permitir consulta por nome utilizando o LIKE e retornando uma Lista

--- src/main/java/br/unitins/model/Otica.java
+++ src/main/java/br/unitins/model/Otica.java
@@ -1,10 +1,15 @@
 package br.unitins.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-@Entity 
-public class Otica extends PanacheEntity {
+@Entity
+public class Otica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String modelo;
     private String marca;
     private String lente;
@@ -14,32 +19,41 @@ public class Otica extends PanacheEntity {
     public String getModelo() {
         return modelo;
     }
+
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
+
     public String getMarca() {
         return marca;
     }
+
     public void setMarca(String marca) {
         this.marca = marca;
     }
+
     public String getLente() {
         return lente;
     }
+
     public void setLente(String lente) {
         this.lente = lente;
     }
+
     public String getFornecedor() {
         return fornecedor;
     }
+
     public void setFornecedor(String fornecedor) {
         this.fornecedor = fornecedor;
     }
+
     public String getTamanho() {
         return tamanho;
     }
+
     public void setTamanho(String tamanho) {
         this.tamanho = tamanho;
     }
-    
+
 }
